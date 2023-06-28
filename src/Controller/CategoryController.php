b172e02be001bfc3793535516eef70d0f0ad6b6a
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-06-28 23:59:06 +0200

    Created Article enitity and its crud

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,8 +43,9 @@ class CategoryController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
-    public function show(Category $category): Response
+    public function show(Category $category, ArticleRepository $articleRepository): Response
     {
+
         return $this->render('category/show.html.twig', [
             'category' => $category,
         ]);
